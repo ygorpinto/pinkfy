aca4012f37e8c2dd6cfd9e7174e08fc9073c476a
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-03-04 21:34:07 -0300

    ok

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -1,13 +1,19 @@
 import { ContainerStyles } from "./ContainerStyles"
 import { PlayContainer } from "../PlayContainer/PlayContainer"
+import { useState } from "react"
 
 export const Container = () => {
+    
+    const [search, setSearch] = useState("");
+    
     return (
         <ContainerStyles>
             <div className="appContainer">
                 <div className="HeaderApp">
                 <h1>Pink Fy.</h1>
                 <input 
+                value={search}
+                onChange={e=>setSearch(e.target.value)}
                 placeholder="Digite o nome da musica"
                 type="text"/>
                 </div>
